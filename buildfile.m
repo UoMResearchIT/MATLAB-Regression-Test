0cc09ad693dc2d7b06bf20bab5141836156e7f25
Martín Herrerías Azcué <martin.herrerias@gmail.com>
2026-08-19 19:26:31 +0100

    build: package infrastructure (toml, buildfile, logo)

---
+++ buildfile.m
@@ -0,0 +1,23 @@
+function plan = buildfile
+import matlab.buildtool.tasks.*
+
+plan = buildplan(localfunctions);
+
+if version('-release') < "2026b"
+    PkgBuild.startup();
+end
+
+plan("clean") = CleanTask;
+plan("check") = CodeIssuesTask;
+plan("test") = TestTask('tests');
+plan("release").Dependencies = ["check" "test"];
+
+plan.DefaultTasks = ["check" "test"];
+end
+
+function releaseTask(~)
+% Package Toolbox using matlab-toml
+
+    disp('Running release task')
+    PkgBuild.fromTOML();
+end
