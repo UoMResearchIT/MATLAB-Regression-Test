fdf6e51e94a1a87be7c3ba9efbd8a07215d4c1fe
Martín Herrerías Azcué <martin.herrerias@gmail.com>
2026-08-24 11:35:32 +0100

    test: WorkspaceBackup test cleanup, add handle-spaces

--- tests/TestWorkspaceBackup.m
+++ tests/TestWorkspaceBackup.m
@@ -4,8 +4,19 @@ function tests = TestWorkspaceBackup()
     tests = functiontests(localfunctions);
 end
 
-function setupOnce(~)
+function setupOnce(testCase)
+
     addpath('..');
+    testCase.TestData.tmpdir = tempname;
+    mkdir(testCase.TestData.tmpdir);
+end
+
+function teardownOnce(testCase)
+
+    fclose all;
+    if isfolder(testCase.TestData.tmpdir)
+        rmdir(testCase.TestData.tmpdir, 's');
+    end
 end
 
 function test_default_constructor(testCase)
@@ -39,16 +50,18 @@ function test_set_file(testCase)
     verifyEqual(testCase, obj.file, 'foo.mat');
 end
 
-function [obj, a, b, c, result] = example(varargin)
+function [obj, a, b, c, result] = example(testCase, varargin)
 % Retrn a test regtest.WorkspaceBackup object, variables to backup, and expected result
 
     % regtest.WorkspaceBackup object
     obj = regtest.WorkspaceBackup('', 'interactive', 0, varargin{:});
     obj.onlynames = {'a','b','c'};
 
+    if ~contains('file', varargin(1:2:end))
     % "personalize" obj.file, so that it is different for each test
-    s = dbstack(1);
-    obj.file = fullfile(tempdir(), s(1).name);
+        name = func2str(testCase.TestFcn);
+        obj.file = fullfile(testCase.TestData.tmpdir, name);
+    end
 
     % Dummy variables
     a = 1;
@@ -62,7 +75,7 @@ end
 function test_constructor(testCase)
 % Just check that setup worked alright
 
-    obj = example();
+    obj = example(testCase);
 
     verifyInstanceOf(testCase, regtest.WorkspaceBackup(), 'regtest.WorkspaceBackup');
     verifyEqual(testCase, obj.interactive, false);
@@ -71,7 +84,7 @@ end
 function test_backup(testCase)
 % General backup (including skipnames)
 
-    [obj, a, b, c, result] = example(); %#ok<ASGLU>
+    [obj, a, b, c, result] = example(testCase); %#ok<ASGLU>
 
     obj.backup();
     verifyTrue(testCase, isfile(obj.file));
@@ -80,7 +93,7 @@ end
 
 function test_dryrun(testCase)
 
-    [obj, a, b, c] = example(); %#ok<ASGLU>
+    [obj, a, b, c] = example(testCase); %#ok<ASGLU>
     [x,vars] = obj.backup(1);
     verifyEqual(testCase, x, obj);
     verifyEqual(testCase, vars, ["a","b","c"]');
@@ -89,8 +102,18 @@ end
 function test_restore(testCase)
 % (partial) restore onto workspace
 
-    [obj, a, b, c, result] = example(); %#ok<ASGLU>
+    [obj, a, b, c, result] = example(testCase); %#ok<ASGLU>
 
+    obj.backup();
+    x = obj.restore();
+    verifyEqual(testCase, x.a, result.a);
+end
+
+function test_file_with_spaces(testCase)
+
+    name = fullfile(testCase.TestData.tmpdir, 'file with spaces.mat');
+    [obj, a, b, c, result] = example(testCase, 'file', name); %#ok<ASGLU>
+    assert(contains(obj.file,' '));
     obj.backup();
     x = obj.restore();
     verifyEqual(testCase, x.a, result.a);
@@ -99,7 +122,7 @@ end
 function test_restore_on_workspace(testCase)
 % (partial) restore onto workspace
 
-    [obj, a, b, c, result] = example(); %#ok<ASGLU>
+    [obj, a, b, c, result] = example(testCase); %#ok<ASGLU>
 
     obj.backup();
     a = a + 1;
@@ -109,7 +132,7 @@ end
 
 function test_skipnames(testCase)
 
-    [obj, a, b, c, result] = example(); %#ok<ASGLU>
+    [obj, a, b, c, result] = example(testCase); %#ok<ASGLU>
 
     obj.skipnames = {'a'}; 
     obj.backup();
@@ -118,7 +141,7 @@ end
 
 function test_onlyclasses(testCase)
 
-    [obj, a, b, c] = example(); %#ok<ASGLU>
+    [obj, a, b, c] = example(testCase); %#ok<ASGLU>
 
     obj.onlyclasses = {'double'}; 
     obj.backup();
@@ -127,7 +150,7 @@ end
 
 function test_skipclasses(testCase)
 
-    [obj, a, b, c, result] = example(); %#ok<ASGLU>
+    [obj, a, b, c, result] = example(testCase); %#ok<ASGLU>
 
     obj.skipclasses = {'cell'}; 
     obj.backup();
@@ -143,7 +166,7 @@ function test_base_backup(testCase)
     assignin('base', var, 42);
 
     % test backup
-    obj = example('workspace','base');
+    obj = example(testCase, 'workspace','base');
     obj.onlynames = var;
     obj.backup();
     verifyEqual(testCase, obj.restore(), struct(var,42))
@@ -157,7 +180,7 @@ end
 function test_backup_by_other(testCase)
 % test backup by third party
 
-    [obj, a, b, c, result] = example(); %#ok<ASGLU>
+    [obj, a, b, c, result] = example(testCase); %#ok<ASGLU>
 
     x = third_party(obj);
     verifyEqual(testCase, x, result)
